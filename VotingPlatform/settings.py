8b9ba18d0ec0f58bf4ef0bf52a2ddbe5d527d57b
Ubuntu <ubuntu@ip-172-31-46-150.ec2.internal>
2015-01-29 01:15:37 +0000

    remove clickjacking header to support redirection

--- VotingPlatform/settings.py
+++ VotingPlatform/settings.py
@@ -55,13 +55,11 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'VotingPlatform.urls'
 
 WSGI_APPLICATION = 'VotingPlatform.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
